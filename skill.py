596d775af495bf8065f1637a8d31d6f6d6417df7
Trevor <tstevenson25@me.com>
2018-01-06 17:30:20 -0500

    Adding finishing touches

--- skill.py
+++ skill.py
@@ -16,7 +16,8 @@ scores = []
 questionVal = 0
 threePtScore = 0
 gameMode = 0
-shotsRemaining = 25
+shotsRemaining = 10
+gameActive = False
 
 #Constants
 
@@ -26,45 +27,39 @@ TIME_LIMIT = 30
 
 @ask.launch
 def launch():
-	prompt = 'Welcome to Mathketball... If this is your first time playing, say, how do I play? Then say start when you are ready to play!'
-	reprompt = 'Say start when you are ready to play!'
-	return question(prompt).reprompt(reprompt)
+        prompt = 'Welcome to Mathketball...What would you like to play?'
+        reprompt = 'Say, start a game with 2 players, when you are ready to play!'
+        return question(prompt).reprompt(reprompt)
 
 @ask.session_ended
 def sessionEnd():
-        print('sessionEnded')
-        return '', 200
+        return '{}', 200
 
 # Intents
 
 @ask.intent('PlayIntent', convert={'Players': int})
 def playGame(Players):
-        if Players:
-                setPlayers(Players)
-                setScores([0]*Players)
-                setGameMode(0)
-                return startRound(1)
-        else:
-                return delegate('')
+        if not Players:
+                Players = 1
+        setPlayers(Players)
+        setScores([0]*Players)
+        setGameMode(0)
+        setGameActive(True)
+        return startRound(1)
     
 @ask.intent('QuestionIntent')
 def getQuestion(Type):
-        print(Type)
+        if not getGameActive():
+                return question('')
+        
         currentTime = time.time()
         if currentTime - getStartTime() > TIME_LIMIT:
-                print('Time is up!')
                 currPlyr = getCurrentPlayer()
                 if currPlyr == getPlayers():
                         return reportScores()
-                return startRound(currPlyr + 1, timesUP=True)
+                return startRound(currPlyr + 1, timesUp=True)
         q = ''
-        if Type == 'free throw':
-                setQuestionVal(1)
-                num1 = random.randint(1,10)
-                num2 = random.randint(1,10)
-                q = str(num1) + ' plus ' + str(num2)
-                setCurrentAnswer(num1+num2)
-        elif Type == '2 pointer':
+        if Type == '2 pointer':
                 setQuestionVal(2)
                 num1 = random.randint(1,10)
                 num2 = random.randint(1,10)
@@ -83,17 +78,26 @@ def getQuestion(Type):
                 num2 = random.randint(1,10)
                 q = 'What is the remainder of {} divided by {}?'.format(num1, num2)
                 setCurrentAnswer(num1%num2)
+        else:
+                setQuestionVal(1)
+                num1 = random.randint(1,10)
+                num2 = random.randint(1,10)
+                q = str(num1) + ' plus ' + str(num2)
+                setCurrentAnswer(num1+num2)
 
         return question(q).reprompt(q)
 
 @ask.intent('AnswerIntent', convert={'Answer' : int})
-def answer(Answer):                
+def answer(Answer):
+        if not getGameActive():
+                return question('')
+        
         if Answer == getCurrentAnswer():
                 if getGameMode() == 0:
                         plyrScores = getScores()
                         plyrScores[getCurrentPlayer()-1] += getQuestionVal()
                         setScores(plyrScores)
-                        return question('Correct').reprompt('Correct')
+                        return question('')
                 elif getGameMode() == 1:
                         plyrScore = getThreePtScore()
                         plyrScore += getQuestionVal()
@@ -103,16 +107,31 @@ def answer(Answer):
         if getGameMode() == 1:
                 return giveMultQuestion()
         
-        return question('Incorrect').reprompt('Incorrect')
+        return question('').reprompt('')
 
 @ask.intent('RulesIntent')
 def giveRules():
-    return question('Mathketball has 2 game modes: game and three point contest. To start a game, say, start a game with 3 players. Each player has {} seconds on the shot clock to answer as many questions as possible...Say free throw, 2 pointer, 3 pointer, or buzzer beater for a question. A free throw is 1 point, a 2 pointer is 2, a 3 pointer is 3, and a buzzer beater is worth 4. The more points it\'s worth, the harder the question! Say, free throw question, and wait for the question. Then answer by saying answer, followed by your answer. The three point contest is single player, and focuses on multiplication. Start the contest by saying, start a three point contest. You will have 25 total questions to get a high score. Each rack has 5 shots, with the last shot a money ball worth 2 points. Good luck, and go Duke!'.format(TIME_LIMIT))
+    return question('Mathketball has 2 game modes: game and three point contest. To start a game, say, start a game with 3 players. Each player has {} seconds on the shot clock to answer as many questions as possible...Say free throw, 2 pointer, 3 pointer, or buzzer beater for a question. A free throw is 1 point, a 2 pointer is 2, a 3 pointer is 3, and a buzzer beater is worth 4. The more points it\'s worth, the harder the question! Say, free throw, and wait for the question. Then give the answer! The three point contest is single player, and focuses on multiplication. Start the contest by saying, start a three point contest. You will have 10 total questions to get a high score. The fifth and tenth shots are money balls, worth 2 points! Good luck!'.format(TIME_LIMIT))
 
 @ask.intent('ThreePointContest')
 def play3PointContest():
         setGameMode(1)
+        setGameActive(True)
         return giveMultQuestion()
+
+@ask.intent('TopFivePlays')
+def topTenPlays():
+        facts = []
+        with open('facts.txt', 'r') as f:
+                for line in f.readlines():
+                        facts.append(line.strip())
+
+        topTen = random.sample(facts, 5)
+        prompt = '<speak><prosody rate="slow">This is Top Five Plays.'
+        for i in range(5,0,-1):
+                prompt += ' Number {}. {}..'.format(i, topTen[i-1])
+        prompt += '</prosody></speak>'
+        return statement(prompt)
 
 @ask.intent('QuitIntent')
 def quitIntent():
@@ -183,6 +202,13 @@ def setShotsRemaining(val):
 def getShotsRemaining():
         return shotsRemaining
 
+def setGameActive(val):
+        global gameActive
+        gameActive = val
+
+def getGameActive():
+        return gameActive
+
 # Game logic
 def startRound(playerNum, timesUp=False):
         setStartTime(time.time())
@@ -193,6 +219,7 @@ def startRound(playerNum, timesUp=False):
         return question(prompt)
 
 def reportScores():
+        setGameActive(False)
         scrs = getScores()
         winner = scrs.index(max(scrs)) + 1
         prompt = 'The scores are in! Player {} has won with a score of {}.'.format(winner, scrs[winner-1])
@@ -224,7 +251,8 @@ def giveMultQuestion():
         return question(prompt).reprompt(prompt)
 
 def quitGame(text = ''):
-	return statement(text + ' Thank you for playing...Come back soon!')
+        setGameActive(False)
+        return statement(text + ' Thank you for playing...Come back soon!')
 
 if __name__ == '__main__':
 	app.run(debug=True)
